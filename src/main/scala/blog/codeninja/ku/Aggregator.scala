1025a2ee91f12f42c9707021df89320a4283f858
Jeffrey Massung <massung@gmail.com>
2017-10-30 00:51:37 -0500

    ActorSubject for aggregator.

--- src/main/scala/blog/codeninja/ku/Aggregator.scala
+++ src/main/scala/blog/codeninja/ku/Aggregator.scala
@@ -7,20 +7,39 @@ import java.io.ByteArrayInputStream
 import java.util.Locale
 import java.util.regex.Pattern
 import monix.execution._
+import monix.execution.atomic._
 import monix.reactive._
 import monix.reactive.subjects._
 import scala.collection.JavaConverters._
 import scala.concurrent.duration._
+import scala.util._
 import scala.util.Try
 import scalaj.http._
-import org.slf4j.{Logger, LoggerFactory}
+import org.slf4j.LoggerFactory
+
+object Aggregator {
+  sealed trait Action
+
+  // enumerated actions for the aggregator
+  final case class Aggregate(url: String, headlines: List[Headline]) extends Action
+
+  // atomic state object for subscribers, headlines, etc.
+  final case class State(
+    val archive: List[String] = List.empty,
+    val feeds: Map[String, List[Headline]] = Map.empty,
+  )
+}
 
 class Aggregator(prefs: Config.Prefs) {
   import Scheduler.Implicits.global
+  import Aggregator._
 
+  // create an initial state for the aggregator
+  private val state = Atomic(new Aggregator.State)
+
+  // slf4j logger
   val logger = LoggerFactory getLogger "Aggregator"
 
-  // parse the maximum age
   val age = {
     val parser = new PeriodFormatterBuilder()
       .appendWeeks().appendSuffix("w")
@@ -40,19 +59,19 @@ class Aggregator(prefs: Config.Prefs) {
     s => Pattern.compile(Pattern.quote(s), Pattern.CASE_INSENSITIVE)
   }
 
-  // create a reactive feed for each url
-  val feeds = prefs.urls map {
-    _ => PublishSubject[List[Headline]]()
+  val feeds = new ActorSubject[Aggregator.Action, Map[String, List[Headline]]](Map.empty) {
+    override def receive(old: Map[String, List[Headline]], msg: Aggregator.Action) = {
+      msg match {
+        case Aggregate(url, hs) => old + (url -> hs)
+      }
+    }
   }
 
-  // pull all the readers together into a single observable
-  val allFeeds = Observable.combineLatestList(feeds: _*)
-
-  // all feeds are an flattened togeter into a sorted list of headlines
-  val headlines = allFeeds map (_.flatten.sorted.filterNot(isOld _).partition(isHidden _))
+  // transform the feeds downloaded into a list of sorted headlines
+  val headlines = feeds map (_.values.flatten.toList.sorted.filterNot(isOld _).partition(isHidden _))
 
   // create a cancelable, periodic reader for all the urls
-  val readers = (prefs.urls zip feeds) map ((aggregate _).tupled)
+  val readers = prefs.urls map (aggregate _)
 
   // stop running the aggregator
   def cancel = readers foreach (_.cancel)
@@ -64,13 +83,16 @@ class Aggregator(prefs: Config.Prefs) {
   def isHidden(h: Headline) = hideFilters.exists(p => p.matcher(h.title).find)
 
   // create a scheduled task that reads the given RSS feed
-  def aggregate(url: String, feed: PublishSubject[List[Headline]]) =
-    Observable.intervalAtFixedRate(1.second, 5.minutes)
-      .flatMap(_ => Try(readFeed(url)) getOrElse Observable.now(List.empty))
-      .foreach(feed onNext _)
+  def aggregate(url: String) =
+    Observable.intervalAtFixedRate(1.second, 5.minutes) foreach { _ =>
+      Try(readFeed(url)) match {
+        case Success(hs) => feeds onNext new Aggregator.Aggregate(url, hs)
+        case Failure(ex) => logger error ex.toString
+      }
+    }
 
   // download the RSS feed, add it to the feed list, and update the view
-  def readFeed(url: String, redirects: Int = 5): Observable[List[Headline]] = {
+  def readFeed(url: String, redirects: Int = 5): List[Headline] = {
     Http(url).timeout(5000, 10000).asBytes match {
       case r if r.isRedirect && redirects > 0 =>
         readFeed(r.location.get, redirects-1)
@@ -83,7 +105,7 @@ class Aggregator(prefs: Config.Prefs) {
         logger info url
 
         // publish a new feed map with new headlines
-        Observable.now(entries.toList)
+        entries.toList
       }
 
       // anything else is an error
